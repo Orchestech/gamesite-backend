6c4be91bcef1b8708fd915874374d2f3a6c8d5b5
Papin Nikolai <nikolai@weirdcat.ru>
2024-02-17 21:45:45 +0300

    Added a test router '/api/account/register'

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Stock routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+//RESTful API
+var accountRegister = require('./routes/api/account/register');
 
 var app = express();
 
@@ -21,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/api/account/register', accountRegister);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
