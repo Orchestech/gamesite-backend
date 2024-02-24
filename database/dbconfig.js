e2222faa1b065643378072fd008382466fc54efb
Papin Nikolai <nikolai@weirdcat.ru>
2024-02-24 13:43:15 +0300

    Added pg-promise and simple configuration for it

---
+++ database/dbconfig.js
@@ -0,0 +1,11 @@
+const username = "";
+const password = "";
+const host = "";
+const port = "";
+const database = ""
+
+const initOptions = {
+    schema: 'public'
+};
+
+module.exports = {username, password, host, port, database, initOptions};
