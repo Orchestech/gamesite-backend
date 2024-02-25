f2c88c2c2a769feaaa994b967e6d5f4ca45417d8
Papin Nikolai <nikolai@weirdcat.ru>
2024-02-25 08:32:12 +0300

    dbconfig.js now uses environment variables instead of a config file

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,8 +1,8 @@
-const username = "";
-const password = "";
-const host = "";
-const port = "";
-const database = ""
+const username = process.env.GAMESITE_DB_USERNAME;
+const password = process.env.GAMESITE_DB_PASSWORD;
+const host = process.env.GAMESITE_DB_HOST;
+const port = process.env.GAMESITE_DB_PORT;
+const database = process.env.GAMESITE_DB_DATABASE;
 
 const initOptions = {
     schema: 'public'
