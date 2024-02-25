aabf7c998a17961aabbc4fbb381937ae4a2ae090
Papin Nikolai <nikolai@weirdcat.ru>
2024-02-25 08:36:07 +0300

    Removed the console.log() of postgres url

--- database/db.js
+++ database/db.js
@@ -2,6 +2,5 @@ var dbcfg = require('./dbconfig');
 const pgp = require('pg-promise')(dbcfg.initOptions);
 
 const db = pgp(`postgres://${dbcfg.username}:${dbcfg.password}@${dbcfg.host}:${dbcfg.port}/${dbcfg.database}`)
-console.log(`postgres://${dbcfg.username}:${dbcfg.password}@${dbcfg.host}:${dbcfg.port}/${dbcfg.database}`)
 
 module.exports = {db, pgp};
