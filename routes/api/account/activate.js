a3c1281d0ab00c046bd36a965a1cc1eead810e37
Papin Nikolai <nikolai@weirdcat.ru>
2024-02-26 18:15:17 +0300

    Implemented account activation

--- routes/api/account/activate.js
+++ routes/api/account/activate.js
@@ -13,14 +13,25 @@ router.post('/', validationRules.accountActivation, async (req, res) => {
         return res.status(400).json({message: "Validation error", errors: errors.array()});
     }
 
-    return res.status(501).json({ message: "Not implemented" });
+    try {
 
-    // try {
-    //     res.status(200).json({ message: "Success" });
-    // } catch (error) {
-    //     console.error(error);
-    //     res.status(500).json({ message: "Error occurred", errors: [{ msg: 'Server error' }] });
-    // }
+        // Check if activation key exists
+        const activationKey = req.query.code;
+        const keys = await db.any('SELECT * FROM activationkeys WHERE key = $1', [activationKey]);
+        if (keys.length === 0) {
+            return res.status(401).json({ message: "Activation key is invalid", errors: [{ msg: 'Invalid activation key' }] });
+        }
+
+        // Activate user and remove activation key
+        const userId = await db.one('SELECT user_id FROM activationkeys WHERE key = $1', [activationKey]);
+        await db.none('UPDATE users SET activated = true WHERE id = $1', [userId]);
+        await db.none('DELETE FROM activationkeys WHERE key = $1', [activationKey]);
+
+        res.status(200).json({ message: "Success" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error occurred", errors: [{ msg: 'Server error' }] });
+    }
 });
 
 module.exports = router;
