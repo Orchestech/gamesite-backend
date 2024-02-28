50878b002cb8b848dbc038134a0d6db49f845116
Papin Nikolai <nikolai@weirdcat.ru>
2024-02-28 22:17:50 +0300

    Slightly changed the name of the environment variable responsible for rate limit time

--- ratelimit/ratelimit.js
+++ ratelimit/ratelimit.js
@@ -5,7 +5,7 @@ const whitelist = (process.env.GAMESITE_RATELIMIT_WHITELIST ?? "::ffff:127.0.0.1
 const limiter = rateLimit({
     skip: (req, res) => whitelist.includes(req.ip),
     //handler: (req) => console.log(req.ip, req.headers['x-forwarded-for']),
-    windowMs: Number(process.env.GAMESITE_RATE_LIMIT ?? 60) * 1000, // 1 minute
+    windowMs: Number(process.env.GAMESITE_RATELIMIT_TIME ?? 60) * 1000, // 1 minute
     max: 5, // limit each IP to 5 requests per windowMs
     message: {"message": "Too many requests, please try again later", errors: [{"msg": "Too many requests, please try again later"}]}
 })
